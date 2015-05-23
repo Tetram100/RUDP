824e5de8c67a7e7a7e5af05e83fc1cf92a013698
tetram <guillaume@dhainaut.fr>
2015-05-23 14:38:44 +0200

    Fonction sendto and co

--- rudp.c
+++ rudp.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,6 +31,13 @@ struct rudp_packet_t {
     char data[RUDP_MAXPKTSIZE];
 } __attribute__((packed));
 
+struct send_packet {
+    struct rudp_packet_t rudp_packet;
+    int len;
+    int counter;
+};
+
+
 int receivePacketCallback(int fd, void *arg);
 
 // TODO Question : est-ce que avoir des variables globales suffit dans notre cas ? -> on n'est censé ne gérer qu'une seule socket par programme.
@@ -41,7 +49,10 @@ int socket_open = 0;
 int receive_set = 0;
 int event_set = 0;
 
-int state;
+int state = 0;
+int sequence_number = 0;
+int window = RUDP_WINDOW;
+struct sockaddr_in6* destination;
 
 /* 
  * rudp_socket: Create a RUDP socket. 
@@ -54,7 +65,7 @@ rudp_socket_t rudp_socket(int port) {
 		int s = socket (AF_INET, SOCK_DGRAM, 0);
 		if(s==-1){
 			printf("Error while opening the socket. Stop sending.\n");
-			return 0;
+			return NULL;
 		}
 
 		struct sockaddr_in s_receiver;
@@ -137,6 +148,80 @@ int rudp_event_handler(rudp_socket_t rsocket,
 
 int rudp_sendto(rudp_socket_t rsocket, void* data, int len, struct sockaddr_in6* to) {
 	
+	if (len > RUDP_MAXPKTSIZE) {
+	    printf("Packet size is too big\n");
+    	return -1;	
+	}
+
+	if (state == CLOSED || state == WAIT_BUFFER || WAIT_FIN_ACK){
+	    printf("Try to send packet in a wrong state\n");
+    	return -1;			
+	} else if (state == LISTEN){
+		struct rudp_packet_t syn_packet;
+		syn_packet.header.version = RUDP_VERSION;
+		syn_packet.header.type = RUDP_SYN;
+		//Plus 1 to avoid a 0 sequence number
+		sequence_number = (rand() % (int)pow(2,32)) +1;
+		syn_packet.header.seqno = sequence_number;
+		destination = to;
+
+		window = window - 1;
+        if (sendto((int) rsocket, (void *) &syn_packet, sizeof (struct rudp_packet_t), 0, (struct sockaddr_in6*) &to, sizeof (struct sockaddr_in6)) < 0) {
+            printf("Failed to send SYN packet\n");
+            return -1;
+        }
+        state = SYN_SENT;
+	}
+
+	//We create the packet
+	struct rudp_packet_t data_packet;
+	data_packet.header.version = RUDP_VERSION;
+	data_packet.header.type = RUDP_DATA;
+	memcpy(data_packet.data, data, len);
+
+	//We save the packet
+	struct send_packet packet;
+	packet.rudp_packet = data_packet;
+	packet.len = len;
+	packet.counter = 0;
+
+	if (window > 0) {
+		send_packet(rsocket, packet);
+	} else {
+		//TODO ajouter le packet à la liste des packets à envoyer
+	}
+
+	return 0;
+}
+
+int send_packet(rudp_socket_t rsocket, struct send_packet packet){
+	window = window -1;
+	if (sendto((int) rsocket, (void *) &packet, packet.len, 0, (struct sockaddr_in6*) destination, sizeof (struct sockaddr_in6)) < 0) {
+		printf("Failed to send SYN packet\n");
+		return -1;
+	}
+	packet.counter++;
+	setTimeOut(packet);
+	return 0;	
+}
+
+int retransmit(int fd, void *arg){
+
+	return 0;
+}
+
+int setTimeOut(struct send_packet packet){
+	struct timeval time_out, t1, t2;
+	time_out.tv_sec = RUDP_TIMEOUT/1000;
+	time_out.tv_usec = (RUDP_TIMEOUT % 1000) * 1000;
+	gettimeofday(&t1, NULL);  
+	timeradd(&t1, &time_out, &t2);
+
+	if(event_timeout(t2, &retransmit, &packet, "timer_callback") == -1) {
+		printf("Error while programming the time out callback\n");
+		return -1;
+	}
+
 	return 0;
 }
 
